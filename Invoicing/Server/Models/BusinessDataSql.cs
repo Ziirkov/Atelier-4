36cb8600a1908888b57851f8e5153386939ee4f9
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2021-01-29 20:25:54 +0100

    Initial Commit
    Initial Commit

---
+++ Invoicing/Server/Models/BusinessDataSql.cs
@@ -0,0 +1,29 @@
+﻿using Invoicing.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Invoicing.Server.Models
+{
+    public class BusinessDataSql : IBusinessData, IDisposable
+    {
+        private SqlConnection cnct;
+        public BusinessDataSql(string connectionString)
+        {
+            cnct = new SqlConnection(connectionString);
+        }
+        public void Dispose()
+        {
+            cnct.Dispose();
+        }
+        public IEnumerable<Invoice> AllInvoices => cnct.Query<Invoice>("SELECT Client AS Customer, Numero AS Reference, Datereglement AS Created, MontantDu AS Amount, MontantRegle AS Paid FROM Factures ORDER BY DateReglement DESC"); //On récupère la liste des factures
+
+        public decimal SalesRevenue => cnct.QuerySingle<decimal>("SELECT SUM(MontantRegle) FROM Factures"); //On récupère ici le chiffe d'affaire 
+
+        public decimal Outstanding => cnct.QuerySingle<decimal>("SELECT SUM(MontantDu+MontantRegle) FROM Factures"); //On récupère le Chiffre d'affaire prévisionnel
+
+       // public Invoice s = new Invoice();
+
+    }
+}
